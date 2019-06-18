a617080517fd245e59130bdfcefddba00775d3dd
Kieran Farr <kieran.farr@gmail.com>
2019-06-18 15:03:29 -0700

    working aframe orthographic component!

--- js/ortho.js
+++ js/ortho.js
@@ -1,11 +1,12 @@
 // https://stackoverflow.com/questions/17558085/three-js-orthographic-camera/17567292#17567292
+// ortho="width: 1; height 2; near: 0.25; far: 3;"
 
 AFRAME.registerComponent('ortho', {
   schema: {
     width: {type: 'number', default: 1.47},
     height: {type: 'number', default: 0.91},
     near: {type: 'number', default: 0.25},
-    far: {type: 'number', default: 3.0}
+    far: {type: 'number', default: 2.0}
   },
   init: function () {
     var sceneEl = this.el.sceneEl;
@@ -22,8 +23,8 @@ AFRAME.registerComponent('ortho', {
     this.orthoCamera = new THREE.OrthographicCamera(
       this.data.width / -2,
       this.data.width / 2,
-      this.data.height / -2,
       this.data.height / 2,
+      this.data.height / -2,
       this.data.near,
       this.data.far);
 
@@ -35,14 +36,16 @@ AFRAME.registerComponent('ortho', {
     threeCamera = sceneEl.camera;
     threeCamera.left = this.data.width / -2;
     threeCamera.right = this.data.width / 2;
-    threeCamera.top = this.data.height / -2;
-    threeCamera.bottom = this.data.height / 2;
+    threeCamera.top = this.data.height / 2;
+    threeCamera.bottom = this.data.height / -2;
     threeCamera.near = this.data.near;
     threeCamera.far = this.data.far;
     threeCamera.updateProjectionMatrix();
+    // document.querySelector("[camera]").object3D.children[1].updateProjectionMatrix()
+    // document.querySelector("[camera]").object3D.children[1].top = 1
   },
   remove: function () {
     this.cameraParent.remove(this.orthoCamera);
-    sceneEl.camera = this.originalCamera;
+    this.el.sceneEl.camera = this.originalCamera;
   }
 });
