e104576e5ac8417afff8f9449c0d8e604664c80c
Kieran Farr <kieran.farr@gmail.com>
2019-06-18 19:27:35 -0700

    mvp calibration controls in state componnet

--- js/state.js
+++ js/state.js
@@ -9,7 +9,11 @@ AFRAME.registerState({
     stop1Peeps: 10,
     stop2Peeps: 0,
     bus1ControllerId: 2,
+    gameRotationY: 0,
+    gamePositionY: 0,
+    worldY: 0,
     menu: 'main',
+    mode: 'client',
     weatherData: {
       name: '',
       temperature: 0,
@@ -45,17 +49,46 @@ AFRAME.registerState({
       state.stop2Peeps += 1;
     },
     increaseBus1ControllerId: function (state, action) {
-      state.Bus1ControllerId += 1;
+      state.bus1ControllerId += 1;
     },
     decreaseBus1ControllerId: function (state, action) {
-      state.Bus1ControllerId -= 1;
-    }
-
-
+      state.bus1ControllerId -= 1;
+    },
+    increaseGameRotationY: function (state, action) {
+      state.gameRotationY += 1;
+    },
+    decreaseGameRotationY: function (state, action) {
+      state.gameRotationY -= 1;
+    },
+    increaseGamePositionY: function (state, action) {
+      state.gamePositionY += 0.001;
+    },
+    decreaseGamePositionY: function (state, action) {
+      state.gamePositionY -= 0.001;
+    },
+    setServerMode: function (state, action) {
+      state.mode = 'server';
+    },
+    setClientMode: function (state, action) {
+      state.mode = 'server';
+    },
   },
   computeState: function (newState, payload) {
     // effective distance (radius) to monitor for door zone collision based on scale of scene
     newState.computedDistance = Number(newState.detectionRadius) / Number(newState.scale);
     newState.computedScaleString = (1 / Number(newState.scale) + " ").repeat(3);
+    newState.computedPositionString = "0 " + Number(newState.gamePositionY) + " 0";
+    newState.computedRotationString = "0 " + Number(newState.gameRotationY) + " 0";
+    // console.log(newState.computedPositionString);
   }
 });
+
+function keyListener(event){
+  // console.log(event.key);
+  if (event.key === "t") {AFRAME.scenes[0].emit('increaseGamePositionY')};
+  if (event.key === "g") {AFRAME.scenes[0].emit('decreaseGamePositionY')};
+  if (event.key === "y") {AFRAME.scenes[0].emit('increaseGameRotationY')};
+  if (event.key === "h") {AFRAME.scenes[0].emit('decreaseGameRotationY')};
+}
+
+window.addEventListener('keypress', keyListener, false);
